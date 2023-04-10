1d64532beeb8ca5d60cfb3c2b06a641858f4e43e
Serhii Zhyhanov <zhiganov76@gmail.com>
2023-04-10 07:23:19 +0300

    fixed js

--- js/menu.js
+++ js/menu.js
@@ -2,11 +2,13 @@
   const refs = {
     openMenuBtn: document.querySelector(".menu-btn"),
     closeMenuBtn: document.querySelector(".mob-menu__btn"),
+    closeMenuLink: document.querySelector(".mob-menu__list"),
     menu: document.querySelector(".mob-menu"),
   };
 
   refs.openMenuBtn.addEventListener("click", toggleMenu);
   refs.closeMenuBtn.addEventListener("click", toggleMenu);
+  refs.closeMenuLink.addEventListener("click", toggleMenu);
 
   function toggleMenu() {
     refs.menu.classList.toggle("is-hidden");
